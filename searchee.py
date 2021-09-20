26e11c5463a59ccac43649e9b034b535f533c0c8
kaiz3n <49936643+abundov@users.noreply.github.com>
2021-09-20 19:10:03 -0400

    Add files via upload

---
+++ searchee.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Searchee v1.0
+# Coded by kaiz3n
+# ~ツ~
+
+
+import os, sys
+
+print("Make sure you have Sublist3r installed, and can open it directly with 'sublist3r ..'!")
+print("Let's run Sublist3r and search for any file at the subdomains found.")
+input1 = input("Which host would you like to scan? ")
+file1 = input("Which file would you like to search for? (Eg, config.json): ")
+command = 'sublist3r -o results.txt -d ' + input1
+os.system(command)
+filepath = 'results.txt'
+with open(filepath) as testy:
+    line = testy.readline()
+    count = 1
+    while line:
+        command1 = 'curl ' + line.strip() + '/' + file1 
+        line = testy.readline()
+        os.system(command1)
+        print("-------------------------------------")
+        print("Match: ".format(line.strip()))
+        print()
+print("Thanks for using Searchee! Now remember, use this information wisely and for the greater good!")
